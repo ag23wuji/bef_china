d8d84e760b5b65878e1181faf22037a34e08a204
godber <godber@datastrategies4change.eu>
2026-01-27 10:36:54 +0100

    added all files

---
+++ schmierblatt.R
@@ -0,0 +1,7 @@
+install.packages("tidyverse") 
+install.packages("rmarkdown")
+bef_china_notebook1 = rmarkdown:::convert_ipynb("/Users/godbergodbersen/Desktop/phd_bef_china/notebook.ipynb")
+bef_china_notebook2 = rmarkdown:::convert_ipynb("/Users/godbergodbersen/Desktop/phd_bef_china/may_23/may_23.ipynb")
+bef_china_notebook3 = rmarkdown:::convert_ipynb("/Users/godbergodbersen/Downloads/Untitled-5.ipynb")
+
+#-> ilex suaveolens 101 in lindera reflexa umgewandelt
